5a8f0d63f444aaf98fdf424fc7009d9e7bf104b2
Simon Jacobs <simonjacobs@simons-air.home>
2021-11-01 21:05:29 +0000

    random_cat and named_cat routes added

--- lib/app.rb
+++ lib/app.rb
@@ -9,8 +9,13 @@ get '/secret' do
   "please leave"
 end
 
-get '/cat' do
-  "<div>
-  <img src='https://i.imgur.com/jFaSxym.png'; style='width:20%; border: 3px dashed #00ffcc; box-shadow: 0 0 20px 10px #ff00cc'>
-  </div>"
+get '/random_cat' do
+  @cat_name = ["biscuits", "dr fluff", "ronny mcpounce"].sample
+  erb(:index)
+end
+
+get '/named_cat' do
+  @cat_name = params[:name]
+  p params
+  erb(:index)
 end
